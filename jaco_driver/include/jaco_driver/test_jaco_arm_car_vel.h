96005e9c326cace426700fa16a5c3d72f58bd222
mpdedonato <mdedonato@ubuntu.ubuntu-domain>
2013-04-21 17:44:44 -0400

    seperated Tf tree

---
+++ jaco_driver/include/jaco_driver/test_jaco_arm_car_vel.h
@@ -0,0 +1,49 @@
+//============================================================================
+// Name        : Jaco.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+/**
+ * @file jaco_arm_control.cpp
+ *
+ * @date   Feb 20, 2013
+ * @author parallels
+ * @brief \todo
+ */
+
+//License File
+#include <geometry_msgs/TwistStamped.h>
+#include <tf/tf.h>
+#include <ros/ros.h>
+#include <dynamic_reconfigure/server.h>
+#include <jaco_driver/JacoDriverConfig.h>
+#include <std_msgs/String.h>
+
+
+
+
+
+class CartesianVelTest {
+public:
+	CartesianVelTest(ros::NodeHandle nh, std::string JointVelocity);
+
+private:
+	void TimerCallback(const ros::TimerEvent&);
+
+	void callback(jaco_driver::JacoDriverConfig &config, uint32_t level);
+	ros::Publisher pub;
+	ros::Timer timer;
+	dynamic_reconfigure::Server<jaco_driver::JacoDriverConfig> dr_server;
+	dynamic_reconfigure::Server<jaco_driver::JacoDriverConfig>::CallbackType dr_call;
+
+	float x_vel ;
+	float y_vel ;
+	float z_vel ;
+	float rx_vel ;
+	float ry_vel ;
+	float rz_vel ;
+
+};
---
+++ jaco_driver/src/nodes/jaco_tf_updater.cpp
@@ -0,0 +1,86 @@
+/*
+ * jaco_tf_updater.cpp
+
+ *
+ *  Created on: Apr 16, 2013
+ *      Author: mdedonato
+ */
+#include <jaco_driver/jaco_tf_updater.h>
+
+
+using namespace jaco;
+
+     JacoTFTree::JacoTFTree(ros::NodeHandle nh, std::string JointAngles)
+    {
+    	this->joint_angles_sub = nh.subscribe(JointAngles, 1, &JacoTFTree::JointAnglesMSG, this);
+    	current_angles.Angle_J1 = 0;
+    	current_angles.Angle_J2 = 0;
+    	current_angles.Angle_J3 = 0;
+    	current_angles.Angle_J4 = 0;
+    	current_angles.Angle_J5 = 0;
+    	current_angles.Angle_J6 = 0;
+    	last_angle_update = ros::Time().now();
+    	this->tf_update_timer = nh.createTimer(ros::Duration(0.01), &JacoTFTree::TFUpdateTimer, this);
+    	tf_update_timer.stop();
+
+    }
+
+    void JacoTFTree::JointAnglesMSG(const jaco_driver::joint_anglesConstPtr& joint_angles) {
+    	current_angles.Angle_J1 = joint_angles->Angle_J1;
+    	current_angles.Angle_J2 = joint_angles->Angle_J2;
+    	current_angles.Angle_J3 = joint_angles->Angle_J3;
+    	current_angles.Angle_J4 = joint_angles->Angle_J4;
+    	current_angles.Angle_J5 = joint_angles->Angle_J5;
+    	current_angles.Angle_J6 = joint_angles->Angle_J6;
+    	last_angle_update = ros::Time().now();
+    	tf_update_timer.start();
+    }
+
+    void JacoTFTree::CalculatePostion(void) {
+
+    	//Update the forward Kinematics
+    	kinematics.UpdateForward(current_angles.Angle_J1, current_angles.Angle_J2, current_angles.Angle_J3,
+    			current_angles.Angle_J4, current_angles.Angle_J5, current_angles.Angle_J6);
+
+    }
+
+    void JacoTFTree::TFUpdateTimer(const ros::TimerEvent&) {
+    	this->CalculatePostion();	//Update TF Tree
+
+    	if((ros::Time().now().toSec()-last_angle_update.toSec())>1)
+    	{
+        	tf_update_timer.stop();
+    	}
+
+    }
+
+
+    int main(int argc, char **argv) {
+
+    	/* Set up ROS */
+    	ros::init(argc, argv, "jaco_tf_updater");
+    	ros::NodeHandle nh;
+    	ros::NodeHandle param_nh("~");
+
+    	std::string JointAngles("JointAngles"); ///String containing the topic name for JointAngles
+
+    	if (argc < 1) {
+    		ROS_INFO( "Usage: jaco_tf_updater joint_angles_topic");
+    		return 1;
+    	} else {
+    		//Grab the topic parameters, print warnings if using default values
+
+    		if (!param_nh.getParam(JointAngles, JointAngles))
+    			ROS_WARN( "Parameter <%s> Not Set. Using Default Joint Angles Topic <%s>!", JointAngles.c_str(), JointAngles.c_str());
+    		  	}
+
+    //Print out received topics
+    	ROS_DEBUG("Got Joint Angles Topic Name: <%s>", JointAngles.c_str());
+
+    	ROS_INFO("Starting Up Jaco TF Updater...");
+
+    //create the arm object
+    	JacoTFTree JacoTF(nh,  JointAngles);
+
+    	ros::spin();
+    }
