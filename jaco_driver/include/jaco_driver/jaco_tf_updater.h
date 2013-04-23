f9f778d20afcb972c5f89da43d723057a77bac5b
mpdedonato <mdedonato@ubuntu.ubuntu-domain>
2013-04-22 21:17:05 -0400

    need this ile

---
+++ jaco_driver/include/jaco_driver/jaco_tf_updater.h
@@ -0,0 +1,41 @@
+/*
+ * jaco_tf_updater.h
+ *
+ *  Created on: Apr 16, 2013
+ *      Author: mdedonato
+ */
+
+#ifndef JACO_TF_UPDATER_H_
+#define JACO_TF_UPDATER_H_
+
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <tf/tf.h>
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
+#include "jaco_driver/joint_angles.h"
+#include <jaco_driver/jaco_arm_kinematics.h>
+#include <time.h>
+
+namespace jaco {
+
+class JacoTFTree {
+public:
+	JacoTFTree(ros::NodeHandle nh, std::string JointAngles);
+
+private:
+	void JointAnglesMSG(const jaco_driver::joint_anglesConstPtr& joint_angles);
+	void CalculatePostion(void);
+	void TFUpdateTimer(const ros::TimerEvent&);
+	jaco::JacoKinematics kinematics;
+	jaco_driver::joint_angles current_angles;
+	ros::Time last_angle_update;
+	ros::Subscriber joint_angles_sub;
+	ros::Timer tf_update_timer;
+
+
+};
+
+}
+
+#endif /* JACO_TF_UPDATER_H_ */
