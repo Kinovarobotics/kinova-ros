e93c27a6202d6c2f0b07fb46bd9a54ec3e1f983a
jpmerc <jean-philippe.mercier.2@ulaval.ca>
2014-05-29 09:27:00 -0400

    fix for finger_action_client
    The package in which it searched for the msgs was the old one

--- jaco_driver/test/finger_action_client.py
+++ jaco_driver/test/finger_action_client.py
@@ -5,15 +5,15 @@ import rospy
 
 import actionlib
 
-import jaco_driver.msg
+import jaco_msgs.msg
 
 import sys
 
 
 def pose_client():
-    client = actionlib.SimpleActionClient('/jaco/finger_joint_angles', jaco_driver.msg.SetFingersPositionAction)
+    client = actionlib.SimpleActionClient('/jaco/finger_joint_angles', jaco_msgs.msg.SetFingersPositionAction)
 
-    goal = jaco_driver.msg.SetFingersPositionGoal()
+    goal = jaco_msgs.msg.SetFingersPositionGoal()
 
     if len(sys.argv) < 4:
         goal.fingers.Finger_1 = 4000
