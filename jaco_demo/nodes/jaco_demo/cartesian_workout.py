Fixed a bug in which the quaternion magnitude was not calculated properly, causing the quaternion orientation fed to the driver to be incorrect.

jaco_demo/nodes/jaco_demo/cartesian_workout.py
@@ -57,7 +57,7 @@ if __name__ == '__main__':
         else:
             print('Using the specified pose:')
             raw_pose = [float(n) for n in sys.argv[2:]]
-            mag = np.sqrt(sum(np.power(raw_pose[4:], 2)))
+            mag = np.sqrt(sum(np.power(raw_pose[3:], 2)))
             poses = [(raw_pose[:3], raw_pose[3:] / mag)]
 
         for pos, orient in poses: