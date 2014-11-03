f0545237ea817f981567bc97cf995f6f71fe5a7c
Martin Pecka <peckama2@fel.cvut.cz>
2014-11-03 18:50:17 +0100

    Improved the gamepad teleoperation node to allow for much greater flexibility in specifying the key mapping.

--- jaco_teleop/nodes/jaco_teleop_joy.py
+++ jaco_teleop/nodes/jaco_teleop_joy.py
@@ -1,95 +1,111 @@
 #!/usr/bin/env python
-# # @file
-# Joystick teleoperation module for the Kinova Jaco arm
-#
-import roslib
 
-roslib.load_manifest('jaco_teleop')
+# Joystick teleoperation module for the Kinova Jaco arm.
+
+import sys
 import rospy
 import actionlib
 from sensor_msgs.msg import Joy
 
 from jaco_msgs.msg import JoystickCommand, SetFingersPositionAction, SetFingersPositionGoal
 from jaco_msgs.srv import Start, Stop;
+from jaco_teleop.joystick_interface import JoystickInterface
 
 from time import sleep
+import yaml
 
-## Joystick teleoperation class
 class JacoTeleopJoy(object):
-    '''Joystick teleoperation interface.
-	'''
+    """
+    Joystick/gamepad teleoperation interface that utilizes the same command that are sent from the dedicated Kinova
+    controller.
+    """
 
-    ## Instantiate a JacoTeleopJoy object
     def __init__(self):
+        """
+        Read joystick config from the YAML config file. Subscribe to the joy, set up publishers and action servers.
+        Required ROS param: ~config_file (string): Path to the YAML config to load.
+        """
+        configStream = open(rospy.get_param('~config_file'), 'r')
+        # This is a list of all config keys we want to pass to the joystick abstraction layer.
+        joystick_config = yaml.load(configStream)
+
+        self.joystick_interface = JoystickInterface(joystick_config)
+
+        rospy.logwarn("Using joystick interface with the following description:\n" + 
+                self.joystick_interface.get_description())
+
+        # Topic that executes all received commands with the real arm.
         self.joystick_topic = "/jaco_arm_driver/in/joystick_command"
         self.joystick_pub = rospy.Publisher(self.joystick_topic, JoystickCommand, queue_size=10)
 
+        # Emergency stop service.
         self.emergency_stop_topic = "/jaco_arm_driver/in/stop"
         rospy.wait_for_service(self.emergency_stop_topic)
         self.emergency_stop = rospy.ServiceProxy(self.emergency_stop_topic, Stop)
 
+        # Emergency stop release service.
         self.emergency_start_topic = "/jaco_arm_driver/in/start"
         rospy.wait_for_service(self.emergency_start_topic)
         self.emergency_start = rospy.ServiceProxy(self.emergency_start_topic, Start)
 
+        # Action server for controlling the fingers.
         self.fingersActionTopic = "/jaco_arm_driver/fingers/finger_positions"
         self.fingersClient = actionlib.SimpleActionClient(self.fingersActionTopic,
                                                           SetFingersPositionAction)
         self.fingersClient.wait_for_server()
 
+        # The joystick topic that "commands" this node.
         rospy.Subscriber('/joy', Joy, self.joyCallBack)
 
-        # hack
-        self.joy_axis2_moved = False
-        self.joy_axis5_moved = False
-
-    ## Listen to the flippers state.
     def joyCallBack(self, joy):
-        '''Listen to the joystick state.'''
+        """
+        Joystick state change callback. Do all the command translation and command the real arm.
+        @param joy: The new joystick state.
+        @type joy: sensor_msgs.msg.Joy
+        """
 
-        if joy.buttons[1] == 1:
+        # First of all, check if the emergency stop is not required, and if it is, call the appropriate service.
+        if self.joystick_interface.get_value(joy, 'emergency_stop') == 1:
             self.emergency_stop()
             rospy.loginfo("Arm stopped.")
             return
 
-        if joy.buttons[3] == 1:
+        # Similarly, perform the emergency stop release if needed.
+        if self.joystick_interface.get_value(joy, 'emergency_release'):
             self.emergency_start()
             rospy.loginfo("Arm started.")
             return
 
-        if not self.joy_axis2_moved and joy.axes[2] != 0:
-            self.joy_axis2_moved = True
-
-        if not self.joy_axis5_moved and joy.axes[5] != 0:
-            self.joy_axis5_moved = True
-
+        # The command that's gonna be sent to the arm.
         command = JoystickCommand()
 
-        command.InclineLeftRight = joy.axes[0]
-        command.InclineForwardBackward = -joy.axes[1]
-        if self.joy_axis2_moved:
-            command.Rotate = (joy.axes[2] - 1) / 2
-        if joy.buttons[4] == 1:
-            command.Rotate = 1
-        command.MoveLeftRight = -joy.axes[3]
-        command.MoveForwardBackward = joy.axes[4]
-        if self.joy_axis5_moved:
-            command.PushPull = (joy.axes[5] - 1) / 2
-        if joy.buttons[5] == 1:
-            command.PushPull = 1
+        # Translate the commands to the arm.
+        command.InclineLeftRight = self.joystick_interface.get_value(joy, 'translate_left_right')
+        command.InclineForwardBackward = self.joystick_interface.get_value(joy, 'translate_forward_backward')
+        command.Rotate = self.joystick_interface.get_value(joy, 'translate_up_down')
 
-        if joy.buttons[7] == 1:
+        command.MoveLeftRight = self.joystick_interface.get_value(joy, 'rotate_x')
+        command.MoveForwardBackward = self.joystick_interface.get_value(joy, 'rotate_y')
+        command.PushPull = self.joystick_interface.get_value(joy, 'rotate_wrist')
+
+        if self.joystick_interface.get_value(joy, 'home_arm') == 1:
             command.ButtonValue[2] = 1
 
+        # Send the built command to the real arm.
         self.joystick_pub.publish(command)
 
+        # Fingers are controlled using the action server. The action server only accepts target finger positions,
+        # so we only need to set either 0 (min) or 65 (max) target values and stop execution when the control is
+        # released.
         fingersTargetPosition = None
-        if joy.axes[7] < -0.1 or joy.buttons[0] == 1:
+        fingersJoyValue = self.joystick_interface.get_value(joy, 'fingers_open_close')
+        if fingersJoyValue < -0.1:
             fingersTargetPosition = 65
-        if joy.axes[7] > 0.1 or joy.buttons[2] == 1:
+        elif fingersJoyValue > 0.1:
             fingersTargetPosition = 0
 
         if fingersTargetPosition is not None:
+            # Send the request to the real arm.
             goal = SetFingersPositionGoal()
             goal.fingers.finger1 = float(fingersTargetPosition)
             goal.fingers.finger2 = float(fingersTargetPosition)
@@ -97,11 +113,11 @@ class JacoTeleopJoy(object):
 
             self.fingersClient.send_goal(goal)
         else:
+            # The control has been released, so immediately stop executing the action.
             self.fingersClient.cancel_all_goals()
 
-## Create a ROS node and instantiate the JacoTeleopJoy class.
 def main():
-    '''Create a ROS node and instantiate the JacoTeleopJoy class.'''
+    """Create a ROS node and instantiate the JacoTeleopJoy class."""
     try:
         rospy.init_node('jaco_teleop_joy')
         jtj = JacoTeleopJoy()
---
+++ jaco_teleop/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(
+    packages=['jaco_teleop'],
+    package_dir={'': 'src'}
+)
+
+setup(**d)
