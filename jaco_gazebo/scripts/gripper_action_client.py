e416bd7c78b1b8e5ac00f702cb1207968438de8d
Plinio Moreno <plinio@isr.tecnico.ulisboa.pt>
2015-05-07 13:41:19 +0200

    Renaming folder to follow the upstream folder name gazebo and moveIt launchers for mico in gazebo tested for the new naming

--- jaco_gazebo/scripts/gripper_action_client.py
+++ jaco_gazebo/scripts/gripper_action_client.py
@@ -4,14 +4,22 @@ import roslib
 import rospy
 from control_msgs.msg import *
 import math
-
+import getopt
 # Brings in the SimpleActionClient
 import actionlib
 
 
-def mico_gripper_client():
+def mico_gripper_client(argv):
     # Creates the SimpleActionClient, passing the type of the action
     # (FibonacciAction) to the constructor.
+    try:
+      opts, args = getopt.getopt(argv,"")
+      if len(args)!=1:
+         print 'gripper_action_client.py <open/close>'
+         return False
+    except getopt.GetoptError:
+      print 'gripper_action_client.py <open/close>'
+      sys.exit(2)
     client = actionlib.SimpleActionClient('gripper_action_server', control_msgs.msg.GripperCommandAction)
 
     # Waits until the action server has started up and started
@@ -20,7 +28,12 @@ def mico_gripper_client():
 
     # Creates a goal to send to the action server.
     goal = control_msgs.msg.GripperCommandGoal()
-    goal.command.position=60.0*math.pi/180.0
+    if args[0]=="close":
+	goal.command.position=60.0*math.pi/180.0
+    elif args[0]=="open":
+	goal.command.position=0.0
+    else:
+	return False
 
     # Sends the goal to the action server.
     client.send_goal(goal)
@@ -36,7 +49,7 @@ if __name__ == '__main__':
         # Initializes a rospy node so that the SimpleActionClient can
         # publish and subscribe over ROS.
         rospy.init_node('mico_gripper_client_py')
-        result = mico_gripper_client()
+        result = mico_gripper_client(sys.argv[1:])
         print "Result:" + str(result)
     except rospy.ROSInterruptException:
         print "program interrupted before completion"
