ad77a99c139a9896aa301a3f497564c5c52fbc22
Unknown <soli@kinova.ca>
2017-02-08 00:35:09 -0500

    Setup gazebo files for 7 dof and mico
    Added inertial parameters to urdf
    Created poisition controller configs
    Added finger controllers

--- kinova_control/config/generate_controller_config.py
+++ kinova_control/config/generate_controller_config.py
@@ -1,11 +1,17 @@
 import yaml
 
-robot_name = 'j2n6s300'
-dof = 6 
+robot_name = 'j2s7s300'
+dof = 7 
+fingers = 3
 robot_joints = []
+finger_joints = []
 
 for i in range(1,dof+1):  
   robot_joints.append(robot_name + '_joint_' + str(i))
+
+for i in range(1,fingers+1):  
+  finger_joints.append(robot_name + '_joint_finger_' + str(i))
+
 
 joint_state_controller = {'joint_state_controller':{'type':'joint_state_controller/JointStateController', 'publish_rate' : 50}}
 
@@ -32,6 +38,18 @@ for joint in robot_joints:
 												             }
   robot_joint_controllers.update(robot_joint_velocity_controller)
 '''
+
+#add finger joint position controllers 
+for joint in finger_joints:
+  finger_joint_position_controller =  { joint + '_position_controller':
+										                     {
+										                     'type':'effort_controllers/JointPositionController',
+										                     'joint':joint,
+										                     'pid': {'p': 100.0, 'i': 0.01, 'd': 10.0}
+										                     }
+										                 }
+  robot_joint_controllers.update(finger_joint_position_controller)
+
 
 config = {'kinova': robot_joint_controllers}
 
