8dcfc79b15fe48f803926a527f0f531884e5d682
Unknown <soli@kinova.ca>
2017-03-08 10:25:53 -0500

    Cleanup/Added parameters to launch flies
    Moved rqt launch to kinova_control
    Added conditional parameters to launch files
    added script to move robot in trajectory

--- kinova_driver/include/kinova_driver/kinova_arm.h
+++ kinova_driver/include/kinova_driver/kinova_arm.h
@@ -111,8 +111,7 @@ class KinovaArm
     void publishJointAngles(void);
     void publishToolPosition(void);
     void publishToolWrench(void);
-    void publishFingerPosition(void);
-    void publishTorques();
+    void publishFingerPosition(void);   
 
     tf::TransformListener tf_listener_;
     ros::NodeHandle node_handle_;
