99753220e130f05d75433fd60e16488b6000b07b
martine1406 <mblouin@kinova.ca>
2018-11-12 13:49:29 -0500

    change push argment default to false in setCartesianPosition
    makes more sense qith the readme: README.md on github stating that "the robot executes the waypoints without stopping".

--- kinova_driver/include/kinova_driver/kinova_comm.h
+++ kinova_driver/include/kinova_driver/kinova_comm.h
@@ -136,7 +136,7 @@ class KinovaComm
     //! \brief send Pose command to robot
     //! \arg position - Pose command
     //! \arg push - If true clears previous trajectories
-    void setCartesianPosition(const KinovaPose &position, int timeout = 0, bool push = true);
+    void setCartesianPosition(const KinovaPose &position, int timeout = 0, bool push = false);
     void setCartesianVelocities(const CartesianInfo &velocities);
     float getMaxTranslationVelocity(void);
     void setMaxTranslationVelocity(const float &max_trans_vel);
